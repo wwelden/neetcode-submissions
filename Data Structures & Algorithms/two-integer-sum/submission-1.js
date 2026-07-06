358c965e279333173fbe1eebf239400cb68c3947
William Welden <99360439+wwelden@users.noreply.github.com>
2026-07-06 17:16:09 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for (let i = 0; i < nums.length; i ++){
+            for (let j = 1; j <= nums.length; j ++){
+                if( nums[i] + nums[j] == target && i != j){ return [i, j]}
+            }
+        }
+    }
+}
